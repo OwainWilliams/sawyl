9877f7644716c25642f4a23dce1c182ca9309588
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-08-28 19:55:19 +0100

    IMP: Search page WIP

--- Models/ContentModels/SearchPageContentModel.cs
+++ Models/ContentModels/SearchPageContentModel.cs
@@ -2,14 +2,29 @@
 using Sawyl.Models.ViewModels;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Web.Common.PublishedModels;
 
 namespace Sawyl.Models.ContentModels
 {
-    public class SearchPageContentModel(IPublishedContent? content) : ContentModel(content)
-    { 
+    public class SearchPageContentModel(IPublishedContent? content) : ContentModel(content), IBannerProperties
+    {
         public SearchRequestModel? SearchRequest { get; set; }
         public SearchResponseModel? SearchResponse { get; set; }
-        public PaginationViewModel? Pagination {  get; set; }
+        public PaginationViewModel? Pagination { get; set; }
+
+        public IHtmlEncodedString? BannerContent => Content.Value<IHtmlEncodedString>("bannerContent");
+        public string? BannerTitle => Content.Value<string>("bannerTitle");
+
+        public IPublishedContentType ContentType => Content.ContentType;
+
+        public Guid Key => Content.Key;
+
+        public IEnumerable<IPublishedProperty> Properties => Content.Properties;
+
+        public IPublishedProperty? GetProperty(string alias)
+        {
+            return Content.GetProperty(alias);
+        }
     }
 }
--- frontend/ts/searchbar.ts
+++ frontend/ts/searchbar.ts
@@ -1,13 +1,21 @@
 const searchBar: HTMLDivElement = document.querySelector('.searchbar') as HTMLDivElement;
+const searchBarForm: HTMLFormElement = document.querySelector('.searchbar-form') as HTMLFormElement;
 const searchBarToggle: HTMLButtonElement = document.querySelector('.searchbarToggle') as HTMLButtonElement;
 const searchBarInput: HTMLInputElement = document.querySelector('.searchbar-form > input') as HTMLInputElement;
+const searchBarSubmit: HTMLButtonElement = document.querySelector('.searchbar-form > .searchbar-button') as HTMLButtonElement;
 
 const toggleSearch: () => void = () => {
     if (searchBar.classList.contains('__active')) {
         searchBar.classList.remove('__active');
         searchBarInput.value = "";
+        searchBarForm.setAttribute('aria-hidden', 'true');
+        searchBarInput.setAttribute('tabindex', '-1');
+        searchBarSubmit.setAttribute('tabindex', '-1');
     } else {
         searchBar.classList.add('__active');
+        searchBarForm.setAttribute('aria-hidden', 'false');
+        searchBarInput.setAttribute('tabindex', '0');
+        searchBarSubmit.setAttribute('tabindex', '0');
         searchBarInput.focus();
     }
 }
