52e3bf30009e7490b5d5b390fed3ead9302407a1
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-08-29 19:08:14 +0100

    IMP: button styles + favicons

--- frontend/ts/searchbar.ts
+++ frontend/ts/searchbar.ts
@@ -4,20 +4,36 @@ const searchBarToggle: HTMLButtonElement = document.querySelector('.searchbarTog
 const searchBarInput: HTMLInputElement = document.querySelector('.searchbar-form > input') as HTMLInputElement;
 const searchBarSubmit: HTMLButtonElement = document.querySelector('.searchbar-form > .searchbar-button') as HTMLButtonElement;
 
+const closeSearch: () => void = () => {
+    searchBar.classList.remove('__active');
+    searchBarInput.value = "";
+    searchBarForm.setAttribute('aria-hidden', 'true');
+    searchBarInput.setAttribute('tabindex', '-1');
+    searchBarSubmit.setAttribute('tabindex', '-1');
+    searchBarToggle.focus();
+}
+
+const openSearch: () => void = () => {
+    searchBar.classList.add('__active');
+    searchBarForm.setAttribute('aria-hidden', 'false');
+    searchBarInput.setAttribute('tabindex', '0');
+    searchBarSubmit.setAttribute('tabindex', '0');
+    searchBarInput.focus();
+}
+
 const toggleSearch: () => void = () => {
     if (searchBar.classList.contains('__active')) {
-        searchBar.classList.remove('__active');
-        searchBarInput.value = "";
-        searchBarForm.setAttribute('aria-hidden', 'true');
-        searchBarInput.setAttribute('tabindex', '-1');
-        searchBarSubmit.setAttribute('tabindex', '-1');
+       closeSearch();
     } else {
-        searchBar.classList.add('__active');
-        searchBarForm.setAttribute('aria-hidden', 'false');
-        searchBarInput.setAttribute('tabindex', '0');
-        searchBarSubmit.setAttribute('tabindex', '0');
-        searchBarInput.focus();
+        openSearch();
     }
 }
 
 searchBarToggle.addEventListener('click', toggleSearch);
+document.body.addEventListener('keyup', (e: KeyboardEvent) => {
+    if (e.key == "Escape") {
+        if (searchBar.classList.contains('__active')) {
+            closeSearch();
+        }
+    }
+});
